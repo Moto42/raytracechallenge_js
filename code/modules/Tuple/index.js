b52686f7195be4aa5810d5943b911ac45c487b79
Moto42 <moto42@gmail.com>
2019-11-25 15:01:47 -0600

    Subtract class function for Tuples working

--- code/modules/Tuple/index.js
+++ code/modules/Tuple/index.js
@@ -16,15 +16,24 @@ Tuple.add = function (a,b){
   const w = a.w + b.w;
   return new Tuple(x,y,z,w);
 }
-
 Tuple.prototype.add = function (b){
   return Tuple.add(this, b);
+}
+
+Tuple.subtract = function (a,b) {
+  const x = a.x - b.x;
+  const y = a.y - b.y;
+  const z = a.z - b.z;
+  const w = a.w - b.w;
+  return new Tuple(x,y,z,w);
+}
+Tuple.prototype.subtract = function (b) {
+  return Tuple.subtract(this, b);
 }
 
 export function Point(x,y,z) {
   Tuple.call(this,x,y,z,1);
 }
-
 export function Vector(x,y,z) {
   Tuple.call(this,x,y,z,0);
 }
