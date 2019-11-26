209443f8db55d3088abd817238f1b14bc6718e6c
Moto42 <moto42@gmail.com>
2019-11-25 21:48:39 -0600

    Document ALL the functions!

--- code/modules/Tuple/index.js
+++ code/modules/Tuple/index.js
@@ -1,5 +1,18 @@
 import {equals} from '../M42Math';
 
+/**
+ * An ordered list of parameters defining either a vector or a point.
+ * x,y,z are self-explanitory.
+ * Remember that the coordinate system is Left Handed with thumb
+ * facing back to you.
+ *
+ * w determines if this is a Vector (w=0) or Point (w=1)
+ * @param       {Number} x a point in the coordinate system or a distance
+ * @param       {Number} y a point in the coordinate system or a distance
+ * @param       {Number} z a point in the coordinate system or a distance
+ * @param       {Number} w determines if this is a Point (1) or a Vector(0)
+ * @constructor
+ */
 export function Tuple(x,y,z,w) {
   this.x = x;
   this.y = y;
@@ -9,6 +22,8 @@ export function Tuple(x,y,z,w) {
   this.isVector = !w;
 }
 
+//check to see if two tuples are "equal" using the 'close enough'
+//floating point check from M42Math
 Tuple.equals = function (a,b){
   const x = equals(a.x, b.x);
   const y = equals(a.y, b.y);
@@ -20,6 +35,18 @@ Tuple.prototype.equals = function (b){
   return Tuple.equals(this, b);
 }
 
+/**
+ * Adds two tuples together
+ *
+ * Note that this may not always make sense in all cases.
+ * - Adding a vector to a point will give you a new point (w=1)
+ * - Adding a Vector to a Vector returns a new vector (w=1)
+ * - However, adding two points gives you... what? (w=2)
+ *
+ * @param  {Tuple} a Tupple to be added.
+ * @param  {Tuple} b Tupple to be added.
+ * @return {Tuple} the sum of Tuples a and b.
+ */
 Tuple.add = function (a,b){
   const x = a.x + b.x;
   const y = a.y + b.y;
@@ -31,6 +58,18 @@ Tuple.prototype.add = function (b){
   return Tuple.add(this, b);
 }
 
+/**
+ * Returns Difference between two tuples
+ *
+ * - Point-Point = Vector between the two points.
+ * - Point-Vector = New Point. Move backwards along the given
+ *   vector from first Point
+ * - Vector - Vector = new Vector. 'change in direction between the two'
+ * - Vector - Point = nonsense(?) w=-1. It's not a point or a Vector.
+ * @param  {Tuple} a Tuple to be suptracted from.
+ * @param  {Tuple} b Tuple to subtract by/with.
+ * @return {Tuple}   Difference between the two Tuples provided
+ */
 Tuple.subtract = function (a,b) {
   const x = a.x - b.x;
   const y = a.y - b.y;
@@ -42,6 +81,11 @@ Tuple.prototype.subtract = function (b) {
   return Tuple.subtract(this, b);
 }
 
+/**
+ * Reverse direction of a vector.
+ * @param  {Tuple} tuple Vector to be negated.
+ * @return {Tuple}       Negated vector (moving in opposite direction)
+ */
 Tuple.negate = function (tuple) {
   const x = tuple.x * -1;
   const y = tuple.y * -1;
@@ -53,6 +97,14 @@ Tuple.prototype.negate = function () {
   return Tuple.negate(this);
 }
 
+/**
+ * Multiply a Vector by a given amount
+ *
+ * "Extend this vector <number> times"
+ * @param  {Tuple} tuple  Vector to be multiplied
+ * @param  {Number} number Scalar toe multiply by
+ * @return {Tuple}        New Tuple, scaled by the appropirate factor
+ */
 Tuple.multiply = function (tuple, number) {
   const x = tuple.x * number;
   const y = tuple.y * number;
@@ -64,6 +116,12 @@ Tuple.prototype.multiply = function (number) {
   return Tuple.multiply(this,number)
 }
 
+/**
+ * divide a vector by a given factor
+ * @param  {Tuple} tuple  Tuple to be divided.
+ * @param  {Number} number The divisor
+ * @return {Tuple}        [description]
+ */
 Tuple.divide = function (tuple, number) {
   const x = tuple.x / number;
   const y = tuple.y / number;
@@ -75,6 +133,11 @@ Tuple.prototype.divide = function (number) {
   return Tuple.divide(this,number)
 }
 
+/**
+ * Find the 'straight line' distance a vector travels.
+ * @param  {Tuple} tuple Vector to be measured.
+ * @return {Number}       the magnitude of the vector
+ */
 Tuple.magnitude = function (tuple) {
   const x = tuple.x;
   const y = tuple.y;
@@ -87,6 +150,16 @@ Tuple.prototype.magnitude = function () {
   return Tuple.magnitude(this)
 }
 
+/**
+ * Convert vectors into a Unit Vector of magnitude 1, preserving direction.
+ *
+ * Vectors can have any random magnitude, and you will want to scale them
+ * down to a 'Unit Vector' of length/magnitude 1, while preserving their direction.
+ *
+ * This does that.
+ * @param  {Tuple} tuple An arbitrary vector
+ * @return {Tuple}       A Unit vector of the same direction.
+ */
 Tuple.normalize = function (tuple) {
   const magnitude = tuple.magnitude();
   if(magnitude === 0) return new Vector(0,0,0);
@@ -99,6 +172,16 @@ Tuple.prototype.normalize = function () {
   return Tuple.normalize(this)
 }
 
+/**
+ * Returns dot-product of two Tuples, (a scalar)
+ *
+ * I dunno how to explain it. Something to do with intersecting rays with spheres.
+ * Go here https://betterexplained.com/articles/vector-calculus-understanding-the-dot-product/ to get started
+ *
+ * @param  {Tuple} a Vector to be dotted
+ * @param  {Tuple} b Vector to be dotted
+ * @return {Number}   Dot product
+ */
 Tuple.dot = function (a,b){
   const x = a.x * b.x;
   const y = a.y * b.y;
@@ -110,6 +193,18 @@ Tuple.prototype.dot = function (b){
   return Tuple.dot(this, b);
 }
 
+/**
+ * Find the Cross Product of two Vectors
+ *
+ * The new vector returned is perpenticular to the plane defined
+ * by the two given vectors.
+ *
+ * Keep in mind that axb != bxa
+ * in fact, one will be the negation of the other.
+ * @param  {Tuple} a First vector to cross
+ * @param  {Tuple} b Second vector to cross
+ * @return {Tuple} The 3D cross product of the given vectors
+ */
 Tuple.cross = function (a,b){
   const x = (a.y * b.z)-(a.z * b.y);
   const y = (a.z * b.x)-(a.x * b.z);
@@ -120,10 +215,23 @@ Tuple.prototype.cross = function (b){
   return Tuple.cross(this, b);
 }
 
-
+/**
+ * shorthand to get a Tuple with w=1. ie: A point.
+ * @param       {Number} x x position of the point.
+ * @param       {Number} y y position of the point.
+ * @param       {Number} z z position of the point.
+ * @constructor
+ */
 export function Point(x,y,z) {
   return new Tuple(x,y,z,1);
 }
+/**
+ * Shorthand to get a Tuple with w=0. ie: a Vector
+ * @param       {Number} x x velocity of the vector
+ * @param       {Number} y y velocity of the vector
+ * @param       {Number} z z velocity of the vector
+ * @constructor
+ */
 export function Vector(x,y,z) {
   return new Tuple(x,y,z,0);
 }
