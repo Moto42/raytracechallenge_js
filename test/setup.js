fb78a06743cf0bf1ae65d49c543e038ce9f3ff08
Moto42 <moto42@gmail.com>
2019-11-25 20:51:27 -0600

    Matcher to test equality of tuples

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,20 @@
 import {equals} from '../code/modules/M42Math';
+import {Tuple} from  '../code/modules/Tuple';
 
+//Passes if the given values are 'close enough'
 expect.extend({
   toFloatingPointEqual(recieved, value) {
     const pass = equals(recieved, value);
+    return {
+      pass: pass,
+    };
+  }
+});
+
+//Passes if the recieved Tuple is equal to the value Tuple
+expect.extend({
+  toEqualTuple(recieved, value) {
+    const pass = Tuple.equals(recieved, value);
     return {
       pass: pass,
     };
